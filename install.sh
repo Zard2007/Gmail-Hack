1f7141c9494dea37d1f5109e6f27a5f9aad366bc
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-07-15 23:48:59 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+g="\033[1;32m"
+r="\033[1;31m"
+b="\033[1;34m"
+w="\033[0m"
+o="\033[1;33m"
+
+cd
+cd
+apt install ruby
+echo -e $w"["$g"INFO"$w"]"$b" Installing Gmail-Hack: Please wait a moment..."$w
+apt install python
+apt install python2
+echo -e $w"["$g"INFO"$w"]"$b" Succesfull installed Gmail-Hack: Trank for installing..."$w
