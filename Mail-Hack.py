d2b05df212e2dac41192ffbf1a17bc7d06ee620a
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2021-10-31 13:29:33 +0200

    Update Mail-Hack.py

--- Mail-Hack.py
+++ Mail-Hack.py
@@ -1,6 +1,16 @@
 import smtplib, sys, os
 from os import system
 
+OKGREEN = '\033[92m'
+WARNING = '\033[0;33m'
+FAIL = '\033[91m'
+ENDC = '\033[0m'
+LITBU = '\033[94m'
+YELLOW = '\033[3;33m'
+CYAN = '\033[0;36'
+colors = ['\033[92m', '\033[91m', '\033[0;33m']
+RAND = random.choice(colors)
+
 GMAIL_PORT = '587'
 
 def artwork():
